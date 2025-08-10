8f25cd49558b90d2c2db0ef094414448bfb28705
도미닉 <kiljh@me.com>
2025-08-10 21:15:48 +0900

    ShotList : 파일 생성, 리액터킷 템플릿 구성

---
+++ Drrrible/Sources/ViewControllers/ShotListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ShotListViewController.swift
+//  Drrrible
+//
+//  Created by 도미닉 on 8/10/25.
+//  Copyright © 2025 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+import ReactorKit
+
+class ShotListViewController: BaseViewController, View {
+    
+    func bind(reactor: ShotListViewReactor) {
+        <#code#>
+    }
+}
---
+++ Drrrible/Sources/ViewControllers/ShotListViewReactor.swift
@@ -0,0 +1,25 @@
+//
+//  ShotListViewReactor.swift
+//  Drrrible
+//
+//  Created by 도미닉 on 8/10/25.
+//  Copyright © 2025 Suyeol Jeon. All rights reserved.
+//
+
+import ReactorKit
+
+class ShotListViewReactor: Reactor {
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    var initialState = State()
+}

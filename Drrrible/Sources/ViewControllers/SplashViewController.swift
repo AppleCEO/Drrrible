7636ff1c886a9fcceef1e43ccf28d7d39858d0c0
도미닉 <kiljh@me.com>
2025-08-10 17:25:03 +0900

    CompositionRoot에서 초기화 하는 코드가 동작하기 위해 init 구현

--- Drrrible/Sources/ViewControllers/SplashViewController.swift
+++ Drrrible/Sources/ViewControllers/SplashViewController.swift
@@ -11,7 +11,21 @@ import RxSwift
 import RxCocoa
 
 final class SplashViewController: BaseViewController, View {
+    private let presentMainScreen: (() -> Void)
+    private let presentLoginScreen: (() -> Void)
     
+    init(reactor: SplashViewReactor,
+         presentLoginScreen: @escaping () -> Void,
+         presentMainScreen: @escaping () -> Void) {
+        self.presentLoginScreen = presentLoginScreen
+        self.presentMainScreen = presentMainScreen
+        super.init()
+        self.reactor = reactor
+    }
+    
+    @MainActor required convenience init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     func bind(reactor: SplashViewReactor) {
         

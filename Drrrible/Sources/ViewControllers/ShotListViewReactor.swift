b692ce4e2f02edf58c34ba0894de3bbd09cff9a0
도미닉 <kiljh@me.com>
2025-08-11 10:06:00 +0900

    ShotList : 리액터 구성

--- Drrrible/Sources/ViewControllers/ShotListViewReactor.swift
+++ Drrrible/Sources/ViewControllers/ShotListViewReactor.swift
@@ -13,21 +13,39 @@ class ShotListViewReactor: Reactor {
     private let shotCellReactorFactory: (Shot) -> ShotCellReactor
     
     enum Action {
-        
+        case showShotList
     }
     
     enum Mutation {
-        
+        case setShots(List<Shot>)
     }
     
     struct State {
-        
+        var shots: List<Shot>
     }
     
-    var initialState = State()
+    var initialState = State(shots: .init(items: []))
     
     init(shotService: ShotService, shotCellReactorFactory: @escaping (Shot) -> ShotCellReactor) {
         self.shotService = shotService
         self.shotCellReactorFactory = shotCellReactorFactory
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .showShotList:
+            return shotService.shots(paging: .refresh)
+                .asObservable()
+                .map { Mutation.setShots($0) }
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case .setShots(let shots):
+            newState.shots = shots
+        }
+        return newState
     }
 }

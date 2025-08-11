bb2e7ffb3d4ed89336a7842d09ccfc85f606975e
도미닉 <kiljh@me.com>
2025-08-11 13:18:06 +0900

    Shot : 파일 생성하여 리액터킷 탬플릿 구성

---
+++ Drrrible/Sources/ViewControllers/ShotViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShotViewController.swift
+//  Drrrible
+//
+//  Created by 도미닉 on 8/11/25.
+//  Copyright © 2025 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import ReactorKit
+
+class ShotViewController: BaseViewController, View {
+    
+    func bind(reactor: ShotViewReactor) {
+        <#code#>
+    }
+}
+
---
+++ Drrrible/Sources/ViewControllers/ShotViewReactor.swift
@@ -0,0 +1,25 @@
+//
+//  ShotViewReactor.swift
+//  Drrrible
+//
+//  Created by 도미닉 on 8/11/25.
+//  Copyright © 2025 Suyeol Jeon. All rights reserved.
+//
+
+import ReactorKit
+
+class ShotViewReactor: Reactor {
+    enum Action {
+        
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    struct State {
+        
+    }
+    
+    var initialState: State = State()
+}
